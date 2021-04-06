938c53494ca2732e8331f7498ae00c3be28516a3
PGreatness <ahnafhasan2000@gmail.com>
2021-04-06 04:49:04 -0400

    finish delete

--- client/src/components/modals/Delete.js
+++ client/src/components/modals/Delete.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { WModal, WMHeader, WMMain, WButton } from 'wt-frontend';
+import WMFooter from 'wt-frontend/build/components/wmodal/WMFooter';
 
 const Delete = (props) => {
-
+    const [isVisible, setVisible] = useState(true);
     const handleDelete = async () => {
         props.deleteList(props.activeid);
         props.setShowDelete(false);
     }
 
     return (
-        <div className="delete-modal">
-            <div className="modal-header" onClose={() => props.setShowDelete(false)}>
-                Delete List?
-			</div>
-
-            <div>
-                <WButton className="modal-button cancel-button" onClick={() => props.setShowDelete(false)} wType="texted">
-                    Cancel
-				</WButton>
-                <label className="col-spacer">&nbsp;</label>
+        <WModal visible={isVisible}>
+            <WMHeader>
+                <div style={{display: "flex"}}>
+                    <div className="delete-text">
+                        Delete List?
+                    </div>
+                </div>
+            </WMHeader>
+            <WMFooter>
+            <div style={{width: "100%", display: "flex", justifyContent: "space-around"}}>
                 <WButton className="modal-button" onClick={handleDelete} clickAnimation="ripple-light" hoverAnimation="darken" shape="rounded" color="danger">
                     Delete
 				</WButton>
+                <label className="col-spacer">&nbsp;</label>
+                <WButton className="modal-button cancel-button" onClick={() => {props.setShowDelete(false); setVisible(false)}} wType="transparent">
+                    Cancel
+				</WButton>
             </div>
-
-        </div>
+            </WMFooter>
+        </WModal>
     );
 }
 
